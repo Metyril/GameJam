49987dd300683573d24a9281dd793983bffa61da
Jeremy090218 <jeremymartin090218@live.fr>
2018-02-06 14:00:32 +0100

    IHM_MenuV5

--- Dungeon/IHM/MenuPrincipal.rb
+++ Dungeon/IHM/MenuPrincipal.rb
@@ -2,18 +2,20 @@
 require 'gosu'  # Librairie graphique Gosu
 
 require_relative './Bouton.rb'
-
+require_relative '../../main.rb'
 
 class MenuPrincipal < Gosu::Window
 
     def initialize(width=1500, height=1500, options={:fullscreen => false, :update_interval => 75})     # options facultatif / update_interval est en ms
       super
       @cursor = Gosu::Image.new('../../media/mouse.png')
+      @titre = Gosu::Image.new('../../media/Omotecy - Titre Final.png')
       @bouton = Bouton.new(500,550,500,100,Gosu::Color::BLUE,"Jouer");
     end
 
     def draw
       @bouton.draw
+      @titre.draw 160, 200, 2
       @cursor.draw self.mouse_x, self.mouse_y, 4
     end
 
@@ -23,8 +25,8 @@ class MenuPrincipal < Gosu::Window
       case id
       when Gosu::MsLeft
         if @mouse_x>@bouton.x && @mouse_x<@bouton.x+@bouton.width && @mouse_y>@bouton.y && @mouse_y<@bouton.y+@bouton.height
-          print("TEST02")
           self.close!
+          Fenetre.new.show
         end
       end
     end
--- main.rb
+++ main.rb
@@ -193,4 +193,4 @@ class Fenetre < Gosu::Window
   end
 end
 
-Fenetre.new.show
+#Fenetre.new.show
