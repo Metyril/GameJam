024684da8001f3a5ac30602bef7799248d74dde0
Jeremy090218 <jeremymartin090218@live.fr>
2018-02-07 12:05:55 +0100

    IHM_Son

--- Dungeon/IHM/MenuPrincipal.rb
+++ Dungeon/IHM/MenuPrincipal.rb
@@ -2,7 +2,7 @@
 require 'gosu'  # Librairie graphique Gosu
 
 require_relative './Bouton.rb'
-#require_relative '../../main.rb'
+require_relative '../../main.rb'
 
 class MenuPrincipal < Gosu::Window
 
@@ -13,14 +13,18 @@ class MenuPrincipal < Gosu::Window
       @bouton = Bouton.new(500,350,270,80,Gosu::Color::CYAN,"Jouer",3)
       @exit = Bouton.new(500,450,270,80,Gosu::Color::CYAN,"Quitter",2.8)
       @music = Gosu::Song.new('../../media/little_apocalypse.ogg')
+      @sound_btn = Bouton.new(1100,500,100,100,Gosu::Color::CYAN,"",2.8)
+      @sound_image = Gosu::Image.new('../../media/sound.png')
+      @music.play(true)
     end
 
     def draw
       @bouton.draw
       @exit.draw
+      @sound_btn.draw
       @titre.draw 50, 100, 2
       @cursor.draw self.mouse_x, self.mouse_y, 4
-      @music.play(true)
+      @sound_image.draw 1110, 510, 2
     end
 
     def update
@@ -36,6 +40,11 @@ class MenuPrincipal < Gosu::Window
       elsif @exit.getColor == Gosu::Color::YELLOW
         @exit.color(Gosu::Color::CYAN)
       end
+      if @sound_btn.isHover(@mouse_x,@mouse_y)
+        @sound_btn.color(Gosu::Color::YELLOW)
+      elsif @sound_btn.getColor == Gosu::Color::YELLOW
+        @sound_btn.color(Gosu::Color::CYAN)
+      end
     end
 
     def button_down(id)
@@ -45,10 +54,15 @@ class MenuPrincipal < Gosu::Window
       when Gosu::MsLeft
         if @bouton.isHover(@mouse_x,@mouse_y)
           self.close!
-          #Fenetre.new.show
+          Fenetre.new.show
         end
         if @exit.isHover(@mouse_x,@mouse_y)
           self.close!
+        end
+        if @sound_btn.isHover(@mouse_x,@mouse_y) && @music.playing?
+          @music.pause
+        elsif @sound_btn.isHover(@mouse_x,@mouse_y) && !@music.playing?
+          @music.play(true)
         end
       end
     end
