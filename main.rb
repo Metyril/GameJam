5c003788b9388032bda2261153a245dc6a0eb987
Jeremy090218 <jeremymartin090218@live.fr>
2018-02-07 14:29:42 +0100

    IHM_MenuPauseV1

--- main.rb
+++ main.rb
@@ -13,6 +13,8 @@ require_relative 'Dungeon/MurHitBox.rb'
 require_relative 'Dungeon/ItemPoing.rb'
 require_relative 'Dungeon/Item.rb'
 
+require_relative 'Dungeon/IHM/Bouton.rb'
+
 WIDTH = 1280
 HEIGHT = 720
 
@@ -83,14 +85,41 @@ class Fenetre < Gosu::Window
 
     #               SUD                 NORD                EST                           OUEST
     @mursHitBox = [MurHitBox.new("S"), MurHitBox.new("N"), MurHitBox.new("E"), MurHitBox.new("W")]
+
+    #MENU pause
+    @pause = false
+    @music = Gosu::Song.new('../../media/little_apocalypse.ogg')
+    @cursor = Gosu::Image.new('../../media/mouse.png')
+    @titre = Gosu::Image.new('../../media/Omotecy - Titre Final.png')
+    @bouton = Bouton.new(500,350,270,80,Gosu::Color::CYAN,"Jouer",3)
+    @exit = Bouton.new(500,450,270,80,Gosu::Color::CYAN,"Quitter",2.8)
+    @sound_btn = Bouton.new(1100,500,100,100,Gosu::Color::CYAN,"",2.8)
+    @sound_image = Gosu::Image.new('../../media/sound.png')
   end
 
   def button_down(id)
     if id == Gosu::KbEscape
-      close
+      @pause = true
     elsif id == Gosu::KB_TAB
       @freeCam = !@freeCam
       @drawTotal = !@drawTotal
+    end
+
+    #MENU PAUSE
+    if id == Gosu::MsLeft
+      @mouse_x = mouse_x.to_i+30
+      @mouse_y = mouse_y.to_i+10
+      if @bouton.isHover(@mouse_x,@mouse_y)
+        @pause = false
+      end
+      if @exit.isHover(@mouse_x,@mouse_y)
+        close
+      end
+      if @sound_btn.isHover(@mouse_x,@mouse_y) && @music.playing?
+        @music.pause
+      elsif @sound_btn.isHover(@mouse_x,@mouse_y) && !@music.playing?
+        @music.play(true)
+      end
     end
   end
 
@@ -218,6 +247,16 @@ class Fenetre < Gosu::Window
     #@ruby.draw(@camera, @mursHitBox[3].x, 0, @mursHitBox[1].z, 0, 0, 0)
     Gosu::draw_rect(0, 0, WIDTH, HEIGHT, 0xff2c3e50, -10000)
     #@map.draw
+
+    #MENU PAUSE
+    if @pause
+      @bouton.draw
+      @exit.draw
+      @sound_btn.draw
+      @titre.draw 50, 100, 2
+      @cursor.draw self.mouse_x, self.mouse_y, 4
+      @sound_image.draw 1110, 510, 2
+    end
   end
 
   def redraw?(x, z)
